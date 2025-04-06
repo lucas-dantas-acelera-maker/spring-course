c46d319df333935a4364d98b341803b647147f20
lucas-dantas-acelera-maker <aluno17@aceleramaker.com.br>
2025-04-06 16:26:37 -0300

    feat: add getProducts method

--- src/main/java/br/com/aceleramaker/springcourse/controller/ProductController.java
+++ src/main/java/br/com/aceleramaker/springcourse/controller/ProductController.java
@@ -4,6 +4,7 @@ import br.com.aceleramaker.springcourse.model.entities.Product;
 import br.com.aceleramaker.springcourse.model.repository.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,4 +21,10 @@ public class ProductController {
         productRepository.save(product);
         return product;
     }
+
+    @GetMapping
+    public Iterable<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
 }
