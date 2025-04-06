d8573e6983cc34fc2e0d0d774bcf4eea1585fb48
lucas-dantas-acelera-maker <aluno17@aceleramaker.com.br>
2025-04-06 14:58:35 -0300

    feat: add ProductController to call ProductRepository for inserting new products into database

---
+++ src/main/java/br/com/aceleramaker/springcourse/controller/ProductController.java
@@ -0,0 +1,27 @@
+package br.com.aceleramaker.springcourse.controller;
+
+import br.com.aceleramaker.springcourse.model.entities.Product;
+import br.com.aceleramaker.springcourse.model.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping
+    public Product addProduct(
+            @RequestParam String name,
+            @RequestParam Double price) {
+
+        Product product = new Product(name, price);
+        productRepository.save(product);
+        return product;
+    }
+}
